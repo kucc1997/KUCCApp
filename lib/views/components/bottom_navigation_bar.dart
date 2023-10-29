51511a1a5682fac134e5a8a0fd4afe4f827200e6
rishadbaniya <rbdroid01@gmail.com>
2023-10-29 21:10:52 +0545

    feat: add a bottom nav bar file

---
+++ lib/views/components/bottom_navigation_bar.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+class BotNavBar extends HookWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text("falsjasj");
+  }
+}
