144b0d4df738953791028f2a21835cc16c89b5d4
Shashinoor13 <shashinoorghimire13@gmail.com>
2023-10-31 21:20:08 +0545

    Add Theme Generator Function --using Figma Colors and Fonts

---
+++ lib/constants/constants.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class Pallet {
+  static const Color textColor = Color(0xFF000000);
+  static const Color primaryColor = Color(0xFF394078);
+  static const Color secondaryColor = Color(0xFFE4E7F7);
+  static const Color backgroundColor = Color(0xFFFFFFFF);
+  static const Color progressColor = Color(0xFF1D7000);
+  static const Color dangerColor = Color(0xFFE86969);
+  static const Color linkColor = Color(0xFF2F89FC);
+}
+
+class Fonts {
+  static const String heading = 'Manrope';
+  static const String subHeading = 'Manrope';
+  static const String body = 'Poppins';
+}
+
+class FontSizes {
+  static const double heading = 24;
+  static const double subHeading = 16;
+  static const double body = 14;
+}
+
+class FontWeights {
+  static const FontWeight bold = FontWeight.w700;
+  static const FontWeight medium = FontWeight.w500;
+  static const FontWeight regular = FontWeight.w400;
+  static const FontWeight light = FontWeight.w300;
+}
+
+class BorderRadii {
+  static const double small = 4;
+  static const double medium = 8;
+  static const double large = 10;
+}
---
+++ lib/constants/theme.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+import 'constants.dart';
+
+ThemeData kTheme() {
+  return ThemeData(
+    colorScheme: ColorScheme.fromSwatch().copyWith(
+      primary: Pallet.primaryColor,
+      secondary: Pallet.secondaryColor,
+      background: Pallet.backgroundColor,
+    ),
+    textTheme: const TextTheme(
+      displayLarge: TextStyle(
+        fontFamily: Fonts.heading,
+        fontSize: FontSizes.heading,
+        fontWeight: FontWeights.bold,
+        color: Pallet.textColor,
+      ),
+      displayMedium: TextStyle(
+        fontFamily: Fonts.heading,
+        fontSize: FontSizes.subHeading,
+        fontWeight: FontWeights.bold,
+        color: Pallet.textColor,
+      ),
+      bodyLarge: TextStyle(
+        fontFamily: Fonts.body,
+        fontSize: FontSizes.body,
+        fontWeight: FontWeights.regular,
+        color: Pallet.textColor,
+      ),
+      bodyMedium: TextStyle(
+        fontFamily: Fonts.body,
+        fontSize: FontSizes.body,
+        fontWeight: FontWeights.regular,
+        color: Pallet.textColor,
+      ),
+    ),
+    buttonTheme: ButtonThemeData(
+      buttonColor: Pallet.primaryColor,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(BorderRadii.medium),
+      ),
+    ),
+  );
+}
