65c21525da17497ad45b1253ac8d37dc7db90f51
rishadbaniya <rbdroid01@gmail.com>
2023-11-03 00:33:03 +0545

    feat: add week slider into routine

--- lib/views/pages/routine/routine.dart
+++ lib/views/pages/routine/routine.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:kucc_app/views/components/app_bar.dart';
+import 'package:kucc_app/views/pages/event_detail/components/event_button.dart';
 import 'package:timelines/timelines.dart';
 
 //return FixedTimeline.tileBuilder(
@@ -106,6 +108,191 @@ import 'package:timelines/timelines.dart';
 //    //    "7" : "PHYSICS 102",
 //    //}
 //}
+
+//class ArrowAnimation extends HookWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    final controller = useAnimationController(duration: const Duration(milliseconds: 200));
+//    final animation = useAnimation(controller);
+//
+//    return AnimatedBuilder(
+//        animation: controller,
+//        builder: (context, child) {
+//          return Transform(
+//              transform: Transform(
+//            alignment: Alignment.center,
+//            transform: Matrix4.identity()
+//              ..setEntry(3, 2, 0.002)
+//              ..rotateX(animation * 3.14),
+//            child: Icon(Icons.keyboard_arrow_up),
+//          ));
+//        });
+//  }
+//}
+
+class RoutineFacultyBatchButton extends HookWidget {
+  const RoutineFacultyBatchButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = useAnimationController(duration: const Duration(milliseconds: 0));
+    final animation = useAnimation(controller);
+    final primaryColor = useState(Colors.white);
+    final textColor = useState(const Color(0xFF394078));
+    // Create an OverlayEntry variable to hold the dropdown widget
+    OverlayEntry? overlayEntry;
+    void showDropDown() {
+      final RenderBox renderBox = context.findRenderObject() as RenderBox;
+      final size = renderBox.size;
+      final offset = renderBox.localToGlobal(Offset(0.0, size.height));
+
+      overlayEntry = OverlayEntry(
+        builder: (context) {
+          return Positioned(
+            top: offset.dy,
+            width: size.width,
+            left: offset.dx,
+            child: Text("alsjdfsalfjasljflsjfsaljfaj"),
+          );
+        },
+      );
+
+      Overlay.of(context)!.insert(overlayEntry!);
+    }
+
+    void hideDropdown() {
+      overlayEntry?.remove();
+      overlayEntry = null;
+    }
+
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onTap: () {
+        hideDropdown(); // Close the dropdown when there's an outside click
+      },
+      child: Material(
+        color: Colors.transparent,
+        elevation: 1,
+        child: InkWell(
+          borderRadius: BorderRadius.all(Radius.circular(8)),
+          onTap: () {
+            //showDropDown();
+            if (controller.isCompleted) {
+              primaryColor.value = Color(0xFF394078);
+              textColor.value = Colors.white;
+              controller.reverse();
+            } else {
+              textColor.value = Color(0xFF394078);
+              primaryColor.value = Colors.white;
+              controller.forward();
+            }
+          },
+          child: Ink(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(12)),
+              color: primaryColor.value,
+            ),
+            width: 300,
+            height: 60,
+            padding: const EdgeInsets.fromLTRB(16, 8, 16, 8),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "CS - 2021",
+                  style: GoogleFonts.manrope(
+                      color: textColor.value, fontSize: 24, fontWeight: FontWeight.w600),
+                ),
+                AnimatedBuilder(
+                  animation: controller,
+                  builder: (context, child) {
+                    return Transform(
+                      alignment: Alignment.center,
+                      transform: Matrix4.identity()
+                        ..setEntry(3, 2, 0.002) // Perspective effect
+                        ..rotateX(animation * 3.14), // Rotate on the X-axis
+                      child: Icon(Icons.keyboard_arrow_up, size: 28, color: textColor.value),
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class RoutineFacultyBatchInput extends HookWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class RoutlineWeekSliderCard extends HookWidget {
+  final String week;
+  RoutlineWeekSliderCard({super.key, required this.week});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      constraints: BoxConstraints.expand(),
+      decoration:
+          BoxDecoration(color: Colors.red, borderRadius: BorderRadius.all(Radius.circular(12))),
+      alignment: Alignment.center,
+      child: Text(
+        week,
+        style: GoogleFonts.manrope(fontSize: 22),
+      ),
+    );
+  }
+}
+
+class RoutineWeekSlider extends HookWidget {
+  @override
+  Widget build(BuildContext context) {
+    final scrollController = useScrollController();
+
+    const weeks = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri"];
+
+    useEffect(() {
+      final totalScrollDistance = scrollController.position.maxScrollExtent;
+      final oneEighthOfDistance = totalScrollDistance / 8;
+      void listener() {
+        if (scrollController.offset >= oneEighthOfDistance) {
+          // Call your function here
+          debugPrint("${scrollController.offset}");
+        }
+      }
+
+      scrollController.addListener(listener);
+    }, []);
+
+    return Container(
+      child: SingleChildScrollView(
+        controller: scrollController,
+        scrollDirection: Axis.horizontal,
+        child: Row(
+          children: [
+            Padding(padding: EdgeInsets.only(left: 120)),
+            ...weeks.map((day) {
+              return Container(
+                width: 120, // Adjust the width as needed
+                height: 120, // Adjust the height as needed
+                padding: EdgeInsets.fromLTRB(28, 8, 28, 8),
+                alignment: Alignment.center,
+                child: RoutlineWeekSliderCard(week: day),
+              );
+            }).toList(),
+            Padding(padding: EdgeInsets.only(right: 120)),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
 class Routine extends HookWidget {
   const Routine({super.key});
 
@@ -119,7 +306,11 @@ class Routine extends HookWidget {
     return Scaffold(
       body: Column(
         children: [
-          KUCCAppBar(),
+          const KUCCAppBar(),
+          const Padding(padding: EdgeInsets.all(20)),
+          const RoutineFacultyBatchButton(),
+          const Padding(padding: EdgeInsets.all(20)),
+          RoutineWeekSlider(),
           Expanded(
             child: Padding(
               padding: EdgeInsets.only(top: 16),
