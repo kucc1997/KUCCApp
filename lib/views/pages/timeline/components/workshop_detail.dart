75effce38b458fd3c7d4fee83899e9de41e0a58f
Rista Shrestha <ristashrestha10@gmail.com>
2023-11-02 07:17:09 +0545

    Edit UI of timeline page

--- lib/views/pages/timeline/components/workshop_detail.dart
+++ lib/views/pages/timeline/components/workshop_detail.dart
@@ -24,7 +24,7 @@ class WorkshopDetail extends HookWidget {
         Text(
           workshopName,
           style: GoogleFonts.inter(
-              textStyle: const TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
+              textStyle: const TextStyle(fontWeight: FontWeight.bold, fontSize: 16.5)),
         ),
         const SizedBox(height: 8),
         Text(
@@ -48,6 +48,6 @@ class WorkshopDetail extends HookWidget {
             ),
           ],
         ),
-    );
+  ]);
   }
 }
