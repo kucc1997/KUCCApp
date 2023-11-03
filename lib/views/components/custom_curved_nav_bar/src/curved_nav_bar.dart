9632b9e37831ea09071d02b71f829eb89e518f31
Shashinoor13 <shashinoorghimire13@gmail.com>
2023-11-03 12:39:12 +0545

    Add: navigation bar with custom curve

---
+++ lib/views/components/custom_curved_nav_bar/src/curved_nav_bar.dart
@@ -0,0 +1,220 @@
+// ignore_for_file: library_private_types_in_public_api
+
+/// `curved_nav_bar.dart`
+///
+/// This file defines a CurvedNavigationBar widget for a Flutter application.
+///
+/// The CurvedNavigationBar is a custom widget that provides a curved design for
+///  bottom navigation bars.
+/// It uses an AnimationController (`_animationController`) to animate transitions
+///  between navigation options.
+///
+/// The `animateTo` method is used to animate the transition to a new position
+///  when a different navigation option is selected.
+/// The duration and curve of the animation can be customized via the widget's
+/// properties (`widget.animationDuration` and `widget.animationCurve`).
+
+import 'package:flutter/material.dart';
+import 'nav_button.dart';
+import 'nav_custom_painter.dart';
+
+typedef _LetIndexPage = bool Function(int value);
+
+class CurvedNavigationBar extends StatefulWidget {
+  final List<String>? labels;
+  final List<Widget> items;
+  final int index;
+  final Color color;
+  final Color? buttonBackgroundColor;
+  final Color backgroundColor;
+  final ValueChanged<int>? onTap;
+  final _LetIndexPage letIndexChange;
+  final Curve animationCurve;
+  final Duration animationDuration;
+  final double height;
+  CurvedNavigationBar({
+    Key? key,
+    required this.items,
+    this.index = 0,
+    this.color = Colors.white,
+    this.buttonBackgroundColor,
+    this.backgroundColor = Colors.blueAccent,
+    this.onTap,
+    _LetIndexPage? letIndexChange,
+    this.animationCurve = Curves.easeOut,
+    this.animationDuration = const Duration(milliseconds: 600),
+    this.height = 75.0,
+    this.labels,
+  })  : letIndexChange = letIndexChange ?? ((_) => true),
+        assert(items.isNotEmpty),
+        assert(0 <= index && index < items.length),
+        assert(0 <= height && height <= 75.0),
+        super(key: key);
+
+  @override
+  CurvedNavigationBarState createState() => CurvedNavigationBarState();
+}
+
+class CurvedNavigationBarState extends State<CurvedNavigationBar>
+    with SingleTickerProviderStateMixin {
+  late double _startingPos;
+  int _endingIndex = 0;
+  late double _pos;
+  double _buttonHide = 0;
+  late Widget _icon;
+  late AnimationController _animationController;
+  late int _length;
+
+  @override
+  void initState() {
+    super.initState();
+    _icon = widget.items[widget.index];
+    _length = widget.items.length;
+    _pos = widget.index / _length;
+    _startingPos = widget.index / _length;
+    _animationController = AnimationController(vsync: this, value: _pos);
+    _animationController.addListener(() {
+      setState(() {
+        _pos = _animationController.value;
+        final endingPos = _endingIndex / widget.items.length;
+        final middle = (endingPos + _startingPos) / 2;
+        if ((endingPos - _pos).abs() < (_startingPos - _pos).abs()) {
+          _icon = widget.items[_endingIndex];
+        }
+        _buttonHide =
+            (1 - ((middle - _pos) / (_startingPos - middle)).abs()).abs();
+      });
+    });
+  }
+
+  @override
+  void didUpdateWidget(CurvedNavigationBar oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.index != widget.index) {
+      final newPosition = widget.index / _length;
+      _startingPos = _pos;
+      _endingIndex = widget.index;
+      _animationController.animateTo(newPosition,
+          duration: widget.animationDuration, curve: widget.animationCurve);
+    }
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return SizedBox(
+      height: widget.height,
+      child: Stack(
+        clipBehavior: Clip.none,
+        alignment: Alignment.bottomCenter,
+        children: <Widget>[
+          Positioned(
+            bottom: -40 - (75.0 - widget.height),
+            left: Directionality.of(context) == TextDirection.rtl
+                ? null
+                : _pos * size.width,
+            right: Directionality.of(context) == TextDirection.rtl
+                ? _pos * size.width
+                : null,
+            width: size.width / _length,
+            child: Center(
+              child: Transform.translate(
+                offset: Offset(
+                  0,
+                  -(1 - _buttonHide) * 80,
+                ),
+                child: Material(
+                  elevation: 9,
+                  color: widget.buttonBackgroundColor ?? widget.color,
+                  type: MaterialType.circle,
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+
+                    /// *[Icon]* is modified here to change the color of selected icon
+                    child: Icon((_icon as Icon).icon, color: Colors.white),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0 - (75.0 - widget.height),
+            child: CustomPaint(
+              painter: NavCustomPainter(
+                  _pos, _length, widget.color, Directionality.of(context)),
+              child: Container(
+                height: 75.0,
+              ),
+            ),
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0 - (75.0 - widget.height),
+            child: SizedBox(
+                height: 100.0,
+                child: Row(
+                    children: widget.items.map((item) {
+                  return NavButton(
+                    onTap: _buttonTap,
+                    position: _pos,
+                    length: _length,
+                    index: widget.items.indexOf(item),
+                    child: Center(child: item),
+                  );
+                }).toList())),
+          ),
+          Positioned(
+            width: size.width / _length,
+            left: Directionality.of(context) == TextDirection.rtl
+                ? null
+                : _pos * size.width,
+            right: Directionality.of(context) == TextDirection.rtl
+                ? _pos * size.width
+                : null,
+            child: Center(
+                child: Padding(
+              padding: const EdgeInsets.only(bottom: 10.0),
+              child: Text(
+                widget.labels![(_pos * _length).round()],
+                style: const TextStyle(
+                  color: Color(0xFF394078),
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            )),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void setPage(int index) {
+    _buttonTap(index);
+  }
+
+  void _buttonTap(int index) {
+    if (!widget.letIndexChange(index)) {
+      return;
+    }
+    if (widget.onTap != null) {
+      widget.onTap!(index);
+    }
+    final newPosition = index / _length;
+    setState(() {
+      _startingPos = _pos;
+      _endingIndex = index;
+      _animationController.animateTo(newPosition,
+          duration: widget.animationDuration, curve: widget.animationCurve);
+    });
+  }
+}
